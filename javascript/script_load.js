48da0934ecbc3285a13aca12840f046ffeee6231
ipmach <andre.garcia.gomez@udc.es>
2017-11-10 17:29:49 +0100

    javascript corrections

---
+++ javascript/script_load.js
@@ -0,0 +1,6 @@
+document.getElementById('about').innerHTML = loadPage('html/about.html');
+document.getElementById('food_drinks').innerHTML = loadPage('html/food_drinks.html');
+document.getElementById('events').innerHTML = loadPage('html/events.html');
+document.getElementById('art').innerHTML = loadPage('html/art.html');
+document.getElementById('volunteer').innerHTML = loadPage('html/volunteer.html');
+document.getElementById('contacts').innerHTML = loadPage('html/contact.html');
