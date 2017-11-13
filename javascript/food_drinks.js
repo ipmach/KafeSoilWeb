4adeb499c1f81b5507cda0dd7af686cc2bd28e3c
lauri3k <32516256+lauri3k@users.noreply.github.com>
2017-11-13 19:55:57 +0100

    fix

--- javascript/food_drinks.js
+++ javascript/food_drinks.js
@@ -15,17 +15,14 @@ signal for allergen food features.*/
 
 function read_array(nameId, array){
    for (var i=0; i<array.length; i++){
-<<<<<<< HEAD
      var table = document.getElementById(nameId);
      var tr = document.createElement("tr");
-	   var itemName = document.createElement("td");
-	   var itemPrice = document.createElement("td");
-=======
+	 var itemName = document.createElement("td");
+	 var itemPrice = document.createElement("td");
 	 var table = document.getElementById(nameId);
      var tr = document.createElement("tr");
 	 var itemName = document.createElement("td");
 	 var itemPrice = document.createElement("td");
->>>>>>> 01c22f86c86cb0bebd2f0450439fc145c885474e
      //let itemAllergen = document.createElement("td");
 	 itemName.appendChild(document.createTextNode(array[i].name));
      itemPrice.appendChild(document.createTextNode(array[i].price));
