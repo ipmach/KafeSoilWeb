dee1d396b66650f9f3f49fb77f7e4daff42da400
Aurianne94 <amjvienney@hotmail.com>
2017-11-13 18:39:36 +0100

    comments

--- javascript/food_drinks.js
+++ javascript/food_drinks.js
@@ -1,3 +1,18 @@
+/*
+
+File name: food_drinks.js
+Written by: Lauri Koikkalainen
+When: November 2017
+Purpose: Fill the Food table and the Drinks table of the html page. 
+
+*/
+
+
+/*For each item in the food list in the JSON file food_drinks.json,
+it creates a new line with two columns, one for the name of the food or drink,
+and one for the price. The commented lines are there in case we would want to 
+signal for allergen food features.*/
+
 function read_array(nameId, array){
    for (var i=0; i<array.length; i++){
 	 var table = document.getElementById(nameId);
@@ -16,3 +31,9 @@ function read_array(nameId, array){
 }
 read_array("food_list",food);
 read_array("drinks_list",drinks)
+
+/*
+Léa Gaigeot, Lauri Koikkalainen, André Garcia Gomez, Aurianne Vienney
+November 2017
+Web Technologies
+*/
